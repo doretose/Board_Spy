5bc79206d3fe9bc5669fee9368fa1602d8b8ace9
unknown <bg1105@naver.com>
2020-01-13 17:42:15 +0900

    1월 13일 라운드매니저 함수 수정, 카드배부 최종

--- Assets/Scripts/TimeCountdown.cs
+++ Assets/Scripts/TimeCountdown.cs
@@ -6,31 +6,40 @@ using TMPro;
 public class TimeCountdown : MonoBehaviour
 {
     public TextMeshProUGUI timeText;
-    public float timeCost;
+    private float timeCost;
     private bool timerBoolean = false;
+    public static bool staticTimerBool = false;
 
     public void startTimer()
     {
-        timeText.gameObject.SetActive(true);
-        timeCost = 20;
-        timerBoolean = true;
+        if (timerBoolean == false)
+        {
+            timeText.gameObject.SetActive(true);
+            timeCost = 20;
+            staticTimerBool = true;
+        }
+        else return;
     }
 
     private void Update()
     {
+        timerBoolean = staticTimerBool;
         if (timerBoolean)
         {
             timeCost -= Time.deltaTime;
             timeText.text = string.Format("{0:0}", timeCost);
 
-            if (timeCost == 0)
+            if (timeCost == 0.0f)
             {
+                this.GetComponent<NetworkRoundManager>().EndTrun();
                 timeText.gameObject.SetActive(false);
-                timerBoolean = false;
-                this.GetComponent<NetworkRoundManager>().();
             }
         }
+        else
+        {
+            timeText.gameObject.SetActive(false);
+            timerBoolean = false;
+        }
+        
     }
-
-
 }
