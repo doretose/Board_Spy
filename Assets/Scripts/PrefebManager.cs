584a72b93d21f4f8670e800ee21a5fbd33743381
ParkBeomgi <bg1105@naver.com>
2020-01-16 00:23:41 +0900

    0115_Round, prefebsX

--- Assets/Scripts/PrefebManager.cs
+++ Assets/Scripts/PrefebManager.cs
@@ -19,11 +19,26 @@ public class PrefebManager : MonoBehaviour
     public static void CreatePrefeb(GameObject isPrefeb, int locX, int locY)
     {
         GameObject go = GameObject.Find(locX + ", " + locY);
-        MeshRenderer mr = go.GetComponent<MeshRenderer>();
         float pos_x = go.transform.position.x;
         float pos_z = go.transform.position.z;
 
         Instantiate(isPrefeb, new Vector3(pos_x, 1, pos_z), isPrefeb.transform.transform.rotation, go.transform);
     }
 
+    public static void DestroyPrefebs(int locX, int locY)
+    {
+        //색깔 초록색으로 변경 해당 타일위의 프리팹을 모두 삭제
+        GameObject endTile = GameObject.Find(locX + ", " + locY);
+        MeshRenderer mr = endTile.GetComponent<MeshRenderer>();
+        mr.material.color = new Color(32 / 255f, 84 / 255f, 30 / 255f);
+
+        //탐색이 끝난 타일 위에 놓인 모든 기물 프리팹을 제거
+        Transform[] endTileChilds;
+        endTileChilds = endTile.GetComponentsInChildren<Transform>(true);
+
+        for (int j = 7; j < endTileChilds.Length; ++j)
+        {
+            Destroy(endTileChilds[j].gameObject);
+        }
+    }
 }
