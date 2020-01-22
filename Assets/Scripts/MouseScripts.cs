bbbf26cce8c68dae16230def84d8964a08381085
JuHyung <doretose@naver.com>
2020-01-22 17:33:12 +0900

    0122 교전창 fix

--- Assets/Scripts/MouseScripts.cs
+++ Assets/Scripts/MouseScripts.cs
@@ -22,6 +22,8 @@ public class MouseScripts : MonoBehaviour
     //public Image used_player;
     //public Image used_card;
     bool war_onoff;
+    GameObject[] objects;
+
     // layMask로 Map타일을 제외하고 Raycast 안되게 처리하기 위한 변수
     int _layerMask;
     void Start()
@@ -97,53 +99,75 @@ public class MouseScripts : MonoBehaviour
                 {
                     choice_Map_x = ourHitObject.GetComponent<Hex>().x;
                     choice_Map_y = ourHitObject.GetComponent<Hex>().y;
-                    GameObject[] objects = GameObject.FindGameObjectsWithTag("war_item");
+                    objects = GameObject.FindGameObjectsWithTag("war_item");
                     if (EventManager.selectTiles[choice_Map_x][choice_Map_y].Count != 0)
                     {
                         if (war_onoff == false)
                         {
+                            Debug.Log("<color=red>107</color>");
+                            result_pannel.SetActive(true);
                             // selectTiles x,y좌표확인후 0초과면 
                             // playerid, cardid
                             // locX, locY, 패널.setActive(true), 
                             //실행되야될 함수 : 패널==> locX, locY(화면 텍스트 위치표시 용도), 
-                            war_onoff = true;
-                            result_pannel.SetActive(true);
-
-
                             for (int i = 0; i < EventManager.selectTiles[choice_Map_x][choice_Map_y].Count; i++)
                             {
                                 Instantiate(War_prefab).transform.SetParent(GameObject.Find("Content").transform, false);
                                 TextMeshProUGUI used_num = War_prefab.transform.GetChild(1).gameObject.GetComponent<TextMeshProUGUI>();
                                 Image used_player = War_prefab.transform.GetChild(2).gameObject.GetComponent<Image>();
                                 Image used_card = War_prefab.transform.GetChild(3).gameObject.GetComponent<Image>();
-                                used_num.text = "" + i;
+                                //0 공백 1 공격 2방어 3매수
+                                used_num.text = $"{i+1}";
                                 switch (EventManager.selectTiles[choice_Map_x][choice_Map_y][i].playerId)
                                 {
-                                    case 0:
+                                    case 1:
                                         {
                                             used_player.sprite = Resources.Load<Sprite>("1");
                                             break;
                                         }
-                                    case 1:
+                                    case 2:
                                         {
                                             used_player.sprite = Resources.Load<Sprite>("2");
                                             break;
                                         }
-                                    case 2:
+                                    case 3:
                                         {
                                             used_player.sprite = Resources.Load<Sprite>("3");
                                             break;
                                         }
-                                    case 3:
+                                    case 4:
                                         {
                                             used_player.sprite = Resources.Load<Sprite>("4");
                                             break;
                                         }
                                 }
-                                //a.sprite = Resources.Load<Sprite>("nameImage/bluffing");
+                                switch (EventManager.selectTiles[choice_Map_x][choice_Map_y][i].cardId)
+                                {
+                                    case 0:
+                                        {
+                                            used_card.sprite = Resources.Load<Sprite>("nameImage/bluffing");
+                                            break;
+                                        }
+                                    case 1:
+                                        {
+                                            used_card.sprite = Resources.Load<Sprite>("nameImage/attack_png");
+                                            break;
+                                        }
+                                    case 2:
+                                        {
+                                            used_card.sprite = Resources.Load<Sprite>("nameImage/sheild_png");
+                                            break;
+                                        }
+                                    case 3:
+                                        {
+                                            used_card.sprite = Resources.Load<Sprite>("nameImage/recruit");
+                                            break;
+                                        }
+                                }
                             }
-
-                            // Item 생성 -> Instantiate(War_prefab).transform.SetParent(GameObject.Find("Content").transform, false);
+                            Debug.Log("<color=red>166</color>");
+                            war_onoff = true;
+                            
                         }
                         else
                         {
